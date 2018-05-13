04b9c4b399f6748652c746f5bdeea715b0f63f94
KiraMelody <jessie_tjq@163.com>
2018-05-13 16:56:11 +0800

    5.13+++

---
+++ 58.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) == 0:
+            return 0
+        s = s.split()
+        if len(s) > 0:
+            return len(s[-1])
+        else:
+            return 0
